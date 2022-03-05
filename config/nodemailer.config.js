c690fc4264f176878d3cd2e357f9154ec81a52d5
bangsiren <bangsir61@gmail.com>
2022-03-05 21:52:28 +0100

    email-updates

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -15,14 +15,19 @@ const transport = nodemailer.createTransport({
 
 module.exports.sendNewDocumentEmail = (type, maillist) => {
     console.log("Check");
-    transport.sendMail({
-      from: user,
-      to: maillist,
-      subject: `New ${type} has been found`,
-      html: `
+    transport
+      .sendMail({
+        from: user,
+        to: maillist,
+        subject: `New ${type} has been found`,
+        html: `
           <h1>Hello</h1>
           <p>Please head over to our website by clicking the link below to check if the documment belongs to you thanks.</p>
           <a href="https://www.missingdocument.cm/">https://www.missingdocument.cm/</a>
           </div>`,
-    }).catch(err => console.log(err));
+      })
+      .catch((err) => {
+        console.log("There was an error sending email");
+        console.log(err);
+      });
 };
